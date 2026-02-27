a0943628a07475002a36821b41a299d6f8dd61ab
tiago santos <xlzthyago@gmail.com>
2026-02-27 08:32:27 -0300

    feat: crearted home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "../components/Header";
+import { TableComponet } from "../components/Table";
+import { CardComponent } from "@/components/Card";
+import TitleSection from "@/components/TitleSection";
+import Footer from "@/components/Footer";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -7,8 +11,24 @@ export const Route = createFileRoute("/")({
 
 function Home() {
   return (
-    <main className="bg-background h-screen">
+    <main className="bg-background ">
       <Header />
+
+      <section className="py-5 m-auto max-w-[300px]  lg:max-w-4xl ">
+        <TitleSection title="Ranking dos melhores" />
+        <div className="mt-4 ">
+          <TableComponet />
+        </div>
+      </section>
+      <section className=" m-auto max-w-4xl  ">
+        <TitleSection title="Modo de Aprendizagem" />
+        <div className="md:flex flex-col px-4 md: px-0 gap-4 md:flex-row my-10">
+          <CardComponent />
+          <CardComponent />
+          <CardComponent />
+        </div>
+      </section>
+      <Footer />
     </main>
   );
 }
