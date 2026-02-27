cf36a260d56a30b26d038e8f3c70641c93c29b40
tiago santos <xlzthyago@gmail.com>
2026-02-27 08:31:57 -0300

    feat:implement componet header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,13 @@
 import { Link } from "@tanstack/react-router";
 export const Header = () => {
   return (
-    <header className="bg-black">
+    <header className="bg-background  shadow-lg">
       <nav className="m-auto max-w-4xl flex items-center justify-between ">
         <Link to="/">
-          <img className="size-20" src="/logo.png" alt="logo" />
+          <img className="size-25" src="/logo.png" alt="logo" />
         </Link>
         <ul>
-          <li>sobre</li>
-
-          <li>contatoi</li>
+          <li className="text-3xl">Tiago</li>
         </ul>
       </nav>
     </header>
