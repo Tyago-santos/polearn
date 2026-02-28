758bbe6ff0098fa5219a96db099f49fba3eca904
tiago santos <xlzthyago@gmail.com>
2026-02-28 19:25:12 -0300

    feat: created component input_field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,18 +1,81 @@
 import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
 
-export function InputField() {
+import type {
+  FieldError,
+  RegisterOptions,
+  UseFormRegister,
+} from "react-hook-form";
+
+import { EyeIcon, EyeClosed } from "lucide-react";
+import { useState } from "react";
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+
+  nivel?: "iniciante" | "intermediário" | "avançado";
+};
+
+type PropsType = {
+  placeholder: string;
+  type: string;
+  label: string;
+  errorValidate?: FieldError;
+  registerType: keyof FormData;
+  register: UseFormRegister<FormData>;
+  rules?: RegisterOptions<FormData, keyof FormData>;
+  passwodShow: boolean | null;
+};
+
+export function InputField({
+  errorValidate,
+  label,
+  placeholder,
+  type,
+  registerType,
+  register,
+  rules,
+  passwodShow,
+}: PropsType) {
+  const [showEye, setShowEye] = useState(false);
+  const inputType =
+    type === "password" ? (showEye ? "text" : "password") : type;
+
   return (
-    <Field>
-      <FieldLabel htmlFor="input-field-username">Username</FieldLabel>
-      <Input
-        id="input-field-username"
-        type="text"
-        placeholder="Enter your username"
-      />
-      <FieldDescription>
-        Choose a unique username for your account.
-      </FieldDescription>
+    <Field className="my-2">
+      <FieldLabel
+        className="text-lg text-black "
+        htmlFor={`input-field-${String(registerType)}`}
+      >
+        {label}
+      </FieldLabel>
+      <div className="flex items-centerm  justify-between border-text-secondary/40 rounded-sm border p-3 shadow-lg w-full ">
+        <input
+          className="outline-none"
+          {...register(registerType, rules)}
+          id={`input-field-${String(registerType)}`}
+          type={inputType}
+          placeholder={placeholder}
+        />
+
+        {passwodShow &&
+          (showEye ? (
+            <EyeIcon
+              onClick={() => setShowEye((prev) => !prev)}
+              className="text-text-secondary"
+            />
+          ) : (
+            <EyeClosed
+              onClick={() => setShowEye((prev) => !prev)}
+              className="text-text-secondary"
+            />
+          ))}
+      </div>
+
+      {errorValidate && (
+        <FieldDescription>{errorValidate.message}</FieldDescription>
+      )}
     </Field>
   );
 }
