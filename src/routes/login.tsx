df36ab2d6a807b8951b7a97ce2a0ff491cc393df
tiago santos <xlzthyago@gmail.com>
2026-02-28 19:23:31 -0300

    feat: ceated routes login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,9 +1,85 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useForm } from "react-hook-form";
 
-export const Route = createFileRoute('/login')({
+import { InputField } from "@/components/InputField";
+import { Button } from "@/components/ui/button";
+import TitleSection from "@/components/TitleSection";
+import LinkNavigation from "@/components/LinkNavigation";
+
+export const Route = createFileRoute("/login")({
   component: RouteComponent,
-})
+});
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 function RouteComponent() {
-  return <div>Hello "/login"!</div>
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const handleSubmitClick = (data: FormData) => {
+    console.log(data.email);
+    console.log(data.name);
+    console.log(data.password);
+  };
+
+  return (
+    <main className="h-screen bg-background flex items-center justify-center px-5">
+      <form
+        className="w-[300px] overflow-y-scroll lg:overflow-y-hidden py-3"
+        onSubmit={handleSubmit(handleSubmitClick)}
+      >
+        <img src="/logo.png" alt="" />
+        <TitleSection title="Faça login" />
+        <InputField
+          type="email"
+          placeholder="exemplo@gmail.com"
+          registerType="email"
+          label="E-mail"
+          register={register}
+          rules={{
+            required: "E-mail é obrigatório",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Digite um e-mail válido",
+            },
+          }}
+          errorValidate={errors.email}
+          passwodShow={false}
+        />
+
+        <Link to="/fogert">Esqueceu a senha</Link>
+
+        <InputField
+          type="password"
+          placeholder="123456"
+          registerType="password"
+          label="Senha"
+          register={register}
+          rules={{
+            required: "Senha é obrigatória",
+            minLength: {
+              value: 6,
+              message: "A senha precisa ter no mínimo 6 caracteres",
+            },
+          }}
+          errorValidate={errors.password}
+          passwodShow={true}
+        />
+
+        <Button>Logar</Button>
+
+        <LinkNavigation
+          describeNavigate="Não possui uma conta? | Faça  cadastro"
+          navigate="/register"
+        />
+      </form>
+    </main>
+  );
 }
