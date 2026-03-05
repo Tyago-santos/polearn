a47b6e85a3a89a6e64efd089f82649a6f17f9d61
Tiago Santos <xlzthyago@gmail.com>
2026-03-05 11:28:41 -0300

    style: style buttons header

--- src/routes/$game.tsx
+++ src/routes/$game.tsx
@@ -1,3 +1,4 @@
+import GameDrag from "@/components/GameDrag";
 import GameQuestion from "@/components/GameQuestion";
 import { createFileRoute, useParams } from "@tanstack/react-router";
 
@@ -12,15 +13,22 @@ function Game() {
     <main className="px-4">
       <header>
         <nav className="max-w-2xl m-auto flex items-center justify-between">
-          <img className="size-35" src="/logo.png" alt="logo" />
+          <img className="size-20 lg:size-35" src="/logo.png" alt="logo" />
 
-          <button className="border-2 border-red-500 hover:bg-red-500 hover:text-white cursor-pointer px-4 py-2 font-bold text-red-500 rounded-full">
-            Desistir
-          </button>
+          <div className="flex gap-4">
+            <button className="border-2 border-red-500 hover:bg-red-500 hover:text-white cursor-pointer  font-bold p-2 text-red-500 rounded-full text-[10px] lg:text-xl lg:px-4 lg:py-2 ">
+              Desistir
+            </button>
+
+            <button className="border-2 border-green-500 hover:bg-green-500 hover:text-white cursor-pointer text-sm  text-[10px] p-2 lg:text-xl lg:px-4 lg:py-2  text-sm font-bold text-green-500 rounded-full">
+              Verificar a ordem
+            </button>
+          </div>
         </nav>
       </header>
 
       {params.game === "question" && <GameQuestion />}
+      {params.game === "drag" && <GameDrag />}
     </main>
   );
 }
