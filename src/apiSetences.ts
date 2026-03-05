38c9d61688e0e8561bbc4b9aa45b1cbcb006288e
Tiago Santos <xlzthyago@gmail.com>
2026-03-05 11:27:05 -0300

    feat: simaulator of  route game drag

---
+++ src/apiSetences.ts
@@ -0,0 +1,54 @@
+const sentences = [
+  {
+    wordsShuffled: ["school", "to", "go", "I"],
+    wordsCorrect: ["I", "go", "to", "school"],
+    correctSentence: "I go to school.",
+  },
+  {
+    wordsShuffled: ["likes", "she", "coffee"],
+    wordsCorrect: ["She", "likes", "coffee"],
+    correctSentence: "She likes coffee.",
+  },
+  {
+    wordsShuffled: ["playing", "are", "they", "soccer"],
+    wordsCorrect: ["They", "are", "playing", "soccer"],
+    correctSentence: "They are playing soccer.",
+  },
+  {
+    wordsShuffled: ["have", "a", "dog", "we"],
+    wordsCorrect: ["We", "have", "a", "dog"],
+    correctSentence: "We have a dog.",
+  },
+  {
+    wordsShuffled: ["reading", "is", "he", "book", "a"],
+    wordsCorrect: ["He", "is", "reading", "a", "book"],
+    correctSentence: "He is reading a book.",
+  },
+  {
+    wordsShuffled: ["blue", "car", "my", "is"],
+    wordsCorrect: ["My", "car", "is", "blue"],
+    correctSentence: "My car is blue.",
+  },
+  {
+    wordsShuffled: ["live", "in", "they", "city", "the"],
+    wordsCorrect: ["They", "live", "in", "the", "city"],
+    correctSentence: "They live in the city.",
+  },
+  {
+    wordsShuffled: ["happy", "am", "I"],
+    wordsCorrect: ["I", "am", "happy"],
+    correctSentence: "I am happy.",
+  },
+  {
+    wordsShuffled: ["teacher", "is", "a", "she"],
+    wordsCorrect: ["She", "is", "a", "teacher"],
+    correctSentence: "She is a teacher.",
+  },
+  {
+    wordsShuffled: ["watching", "are", "movie", "we", "a"],
+    wordsCorrect: ["We", "are", "watching", "a", "movie"],
+    correctSentence: "We are watching a movie.",
+  },
+];
+
+export default sentences;
